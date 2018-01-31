0370212e4375a0f055ecde4e49ddb56a5afcbdf9
aptalca <aptalca@users.noreply.github.com>
2018-01-31 16:16:29 -0500

    Initial release

---
+++ root/app/le-renew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bash
+
+. /config/donoteditthisfile.conf
+
+echo "<------------------------------------------------->"
+echo
+echo "<------------------------------------------------->"
+echo "cronjob running on "$(date)
+echo "Running certbot renew"
+if [ "$ORIGVALIDATION" = "dns" ]; then
+  certbot -n renew
+else
+  certbot -n renew --pre-hook "[[ ! -z $(ps -ef | grep nginx: | grep -v grep) ]] && s6-svc -d /var/run/s6/services/nginx" --post-hook "[[ ! -z $(ps -ef | grep "s6-supervise nginx" | grep -v grep) ]] && s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+fi
